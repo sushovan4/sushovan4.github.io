ad8283cfae554f12081e45dc9a541fc1e06b952e
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2024-10-29 22:43:08 +0000

    Built site for gh-pages

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 window.addEventListener('load', (event) => {
-    let path = window.location.pathname.split('/')[1];
+    let path = window.location.pathname.split('/');
     elms = document.getElementsByClassName("nav-link");
-    console.log(path);    
     
     for(let i = 0; i < elms.length; i++) {
         let text = elms[i].textContent.toLowerCase().trim();
-        if(text == path) {
+        if(text == path[1] && path[2] != 'publications') {
             elms[i].classList.add("active");
             break;
         }
